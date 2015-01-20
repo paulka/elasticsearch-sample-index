093b954641978b2410e2812969e90b541cff8eae
paulka <pkavanagh@shopkeep.com>
2015-01-20 11:09:40 +0000

    Adding a much bigger dataset, and field names.

--- elasticput.php
+++ elasticput.php
@@ -1,10 +1,10 @@
 <?php
 // CONFIGURE BELOW
 $esHostProtocol = 'http'; // could be https, but may require changes to CURL opts?
-$esHost = 'localhost';
+$esHost = 'elasticsearch.platform.staging.posrip.com';
 $esPort = '9200';
-$indexName = 'comicbook';
-$docType = 'superhero';
+$indexName = 'sk-loadtest-001';
+$docType = 'birdstrike';
 $documentId = 1;
 
 // DO NOT EDIT BELOW THIS LINE!
@@ -13,13 +13,84 @@ if (($handle = fopen('indexme.csv', "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 300, ",")) !== FALSE) {
         $num = count($data);
         $row++;
-        $hero = $data[0];
-        $summary = $data[1];
-        echo 'INDEXING ROW: ' . $row . ' HERO: ' . $hero . "\n";
+        $acrafttype = $data[0];
+	$airportname = $data[1];
+        $alt = $data[2];
+        $craftmodel = $data[3];
+        $numberstruck = $dat[4];
+        $effect = $data[5];
+        $effectother = $data[6];
+        $location = $data[7];
+        $flightnumber = $data[8];
+        $date = $data[9];
+        $recid = $data[10];
+        $effectdamage = $data[11];
+        $locationother= $data[12];
+        $enginecount = $data[13];
+        $airline = $data[14];
+        $originstate = $data[15];
+        $flightpahse = $data[16];
+	$conditions = $data[17];
+        $wildliferemainscollected = $data[18];
+        $wildlifesenttosmithsonian = $data[19];
+        $remarks = $data[20];
+        $reporteddate = $data[21];
+        $wildlifesize = $data[22];
+        $conditionssky = $data[23];
+        $wildlifespecies = $data[24];
+        $time = $data[25];
+        $timeoday = $data[26];
+        $pilotwarned = $data[27];
+        $aircraftoutofservicehours = $data[28];
+        $costadjusted = $data[29];
+        $costrepair = $data[30];
+        $costtotal = $data[31];
+        $milesfromairport = $data[32];
+        $feetaboveground = $data[33];
+        $humanfatalities = $data[34];
+        $humaninjuries = $data[35];
+        $ias = $data[36];
+
+        echo 'INDEXING ROW: ' . $row . ' RECORD ID: ' . $recid . "\n";
 
         $json_data = array(
-            "name" => $hero,
-            "summary" => $summary
+              "aircraft_type" => $acrafttype,
+              "airport_name" => $airportname,
+              "altitude" => $alt,
+              "aircraft_make_model" => $craftmodel,
+              "wildlife_number_struck" => $numberstruck,
+              "effect_impact_on_flight" => $effect,
+              "effect_other" => $effectother,
+              "location_nearby_if_enroute" => $location,
+              "aircraft_flight_number" => $flightnumber,
+              "flight_date" => $date,
+              "record_id" => $recid,
+              "effect_indicated_damage" => $effectdamage,
+              "location_freeform_en_route" => $locationother,
+              "aircraft_number_of_engines" => $enginecount,
+              "aircraft_airline_or_operator" => $airline,
+              "origin_state" => $originstate,
+              "when_flight_phase" => $flightpahse,
+              "conditions_precipitation" => $conditions,
+              "wildlife_collected" => $wildliferemainscollecte,
+              "wildlife_sent_to_smithsonian" => $wildlifesenttosmithsonia,
+              "remarks" => $remarks,
+              "reported_date" => $reporteddate,
+              "wildlife_size" => $wildlifesize,
+              "conditions_sky" => $conditionssky,
+              "wildlife_species" => $wildlifespecies,
+              "when_time" => $time,
+              "when_time_of_day" => $timeoday,
+              "pilot_warned" => $pilotwarned,
+              "cost_aircraft_hours_out_of_service" => $aircraftoutofservicehour,
+              "cost_other" => $costadjusted,
+              "cost_repair" => $costrepair,
+              "cost_total" => $costtotal,
+              "location_miles_from_airport" => $milesfromairport,
+              "feet_above_ground" => $feetaboveground,
+              "human_fatalities" => $humanfatalities,
+              "human_injuries" => $humaninjuries,
+              "ias" => $ias	      
         );
         $jsonData = json_encode($json_data);
 
